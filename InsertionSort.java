4c62136c665f66b5dc1ba4e897ef98314040f718
Diaz David <davidadiaz117@gmail.com>
2024-09-10 12:50:59 -0600

    Avance Merge, insertion, selection

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,9 +1,20 @@
 package algoritmos;
-
+import utilerias.Utilerias;
 public class InsertionSort{
-    public static 
-    public static void insertionSort(){
 
+    public static void insertionSort(int[] array){
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+                Utilerias.plusComparaciones();
+            }  
+            array[i+1] = key; 
+            Utilerias.plusInserciones();   
+        } 
     }
 }
 
---
+++ SelectionSort.java
@@ -0,0 +1,18 @@
+package algoritmos;
+import utilerias.Utilerias;
+
+public class SelectionSort{
+
+    public static void selectionSort(int[] arr){  
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++){  
+            int min = i;  
+            for (int j = i + 1; j < n; j++){  
+                if (arr[j] < arr[min]){  
+                    min = j;  
+                }        
+            }  
+            Utilerias.swap(arr, i,min);
+        }  
+    }  
+}
